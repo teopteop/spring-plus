580af0e1e6b16680a5ae42cebec79c785e82bda6
teopteop <teopteop0718@gmail.com>
2025-06-30 17:16:02 +0900

    refactor: 프로젝션 사용을 위한 코드 수정
    전체 생성자 추가,
    of 메서드 어노테이션에서 분리

--- src/main/java/org/example/expert/domain/user/dto/response/UserResponse.java
+++ src/main/java/org/example/expert/domain/user/dto/response/UserResponse.java
@@ -4,11 +4,20 @@ import lombok.AllArgsConstructor;
 import lombok.Getter;
 
 @Getter
-@AllArgsConstructor(staticName = "of")
 public class UserResponse {
 
     private final Long id;
     private final String email;
     private final String nickname;
 
+    //프로젝션 사용을 위한 코드 수정
+    public UserResponse(Long id, String email, String nickname) {
+        this.id = id;
+        this.email = email;
+        this.nickname = nickname;
+    }
+
+    public static UserResponse of(Long id, String email, String nickname) {
+        return new UserResponse(id, email, nickname);
+    }
 }
